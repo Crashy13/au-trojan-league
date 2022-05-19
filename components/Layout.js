f9ed14b16580e17d1e9aae022a05587cb566bea3
Scotty Wyatt <crashy@scottys-mbp.attlocal.net>
2022-05-19 10:04:32 -0400

    changed layout to work better for mobile

--- components/Layout.js
+++ components/Layout.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div className='h-full bg-slate-300 w-full'>
-        <header className='bg-yellow-600 uppercase text-white text-2xl py-6'>
-            <nav>
-                <div className='flex items-center justify-center gap-10'>
-                        <Link href='/'>Home</Link>
-                        <Link href='/HistoricalStandings'>Historical Standings</Link>
-                        <Link href='/CurrentDraftAndTrades'>Drafts and Trades</Link>
-                        <Link href='/RuleChanges'>Rule Changes and End-Season Rosters</Link>
-                        <Link href='/Keepers'>Keeper Claims and XFL</Link>
-                </div>
-            </nav>
+    <>
+      <div className="h-full bg-slate-300 w-full">
+        <header>
+          <nav
+            className="bg-yellow-600 uppercase text-white text-2xl py-6"
+          >
+            <div className="lg:inline-flex lg:w-auto mt-2 lg:mt-0">
+              <ul className="md:mx-40 flex flex-col md:flex-row md:space-x-10 space-y-2 md:space-y-0">
+                <li className="hover:text-black focus:text-black">
+                  <Link href="/">Home</Link>
+                </li>
+                <li className="hover:text-black focus:text-black">
+                  <Link href="/HistoricalStandings">Historical Standings</Link>
+                </li>
+                <li className="hover:text-black">
+                  <Link href="/CurrentDraftAndTrades">Drafts and Trades</Link>
+                </li>
+                <li className="hover:text-black">
+                  <Link href="/RuleChanges">
+                    Rule Changes and End-Season Rosters
+                  </Link>
+                </li>
+                <li className="hover:text-black">
+                  <Link href="/Keepers">Keeper Claims and XFL</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </header>
         <main>{children}</main>
-        <footer className='bg-black text-purple-600'>
-            <div className='flex items-center justify-center'>
-                <p>Created by the resident Dark Lord</p>
-            </div>
+        <footer className="bg-black text-purple-600">
+          <div className="flex items-center justify-center">
+            <p>Created by the resident Dark Lord</p>
+          </div>
         </footer>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
