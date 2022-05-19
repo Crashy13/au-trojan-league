59db87385b52c03815729c2e2b2d1e07763c89c9
Scotty Wyatt <crashy@scottys-mbp.attlocal.net>
2022-05-19 11:59:34 -0400

    changed formatting of keepers

--- pages/Keepers.js
+++ pages/Keepers.js
@@ -3,12 +3,12 @@ import { keeper_picks } from "../public/data/keepers";
 
 const keepers = () => {
   return (
-    <div>
+    <div className="md:text-center mb-10">
       <>
         {keeper_picks.map((keeper) => {
           return (
             <>
-              <h1 className='text-2xl font-bold my-4'>{keeper.name}</h1>
+              <h1 className='text-2xl font-bold my-4 text-yellow-600'>{keeper.name}</h1>
               <h2 className='text-xl'>2021 Keepers</h2>
               <p>{keeper.players_kept}</p>
               <h2 className="text-xl mt-2">XFL Players</h2>
